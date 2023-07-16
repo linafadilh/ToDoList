bd4d041f8dd75db3151fbe3dad6941e3397ebc1f
Lina Fadilah <linahardiyana@gmail.com>
2023-07-17 02:11:22 +0900

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Lina Fadilah on 7/17/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
